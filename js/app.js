0b22c7afcad4b0afc79b2a52dabf7577ff5dc5fa
Michael Fitzgerald <mikitz@users.noreply.github.com>
2022-03-26 20:53:23 +0900

    streamlining

--- js/app.js
+++ js/app.js
@@ -7,12 +7,11 @@ function populateSideNav() {
             let button = document.createElement('button') // Create the button
             button.id = htmlPage // Set its ID
             button.classList.add('dropdown-btn') // Append the appropriate class
-            // button.addEventListener('click', function() { populateContent(htmlPage) }) // Set a click listener
             button.addEventListener('click', function() { buildPage(this) })
             // Icon
             let icon = document.createElement('i')
             icon.setAttribute('class', element.ICON)
-            icon.style.marginRight = '5px'
+            icon.style.marginRight = '10px'
             icon.style.textAlign = 'right'
             button.appendChild(icon)
             // Inner Text
@@ -21,27 +20,6 @@ function populateSideNav() {
             button.appendChild(span)
             // Append button to the side navigation
             sideNavElement.appendChild(button)
-    });
-}
-// Function to populate the homebrew content
-function populateContent(htmlPage) {
-    let content = document.getElementById('content') // Get the content element that gets populated with the proper HTML
-    // Active Class Toggle
-    pages.forEach(element => {
-        let ID = (element.NAME).replaceAll(" ", "_").toLowerCase() // Format it to pull the proper file
-        if ( ID == htmlPage ) {
-            document.getElementById(ID).classList.add('active') // Set active class
-        } else {
-            document.getElementById(ID).classList.remove('active') // Set active class
-        }
-    });
-    $(content).load(`html/${htmlPage}.html`) // Populate the innerHTML based on the selected method
-}
-// Function to set the listeners
-function indexListeners() {
-    let topNavCheckboxes = document.getElementsByName('top-nav')
-    topNavCheckboxes.forEach(element => {
-        element.addEventListener('change', function() { populateContent() })
     });
 }
 // Function to build each page
